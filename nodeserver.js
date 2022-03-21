4a23f826fa4d7fc379ee153bf54a89b610159733
Sunaada Hebbar <shebbarm@asu.edu>
2022-03-21 00:58:03 -0700

    adding source files

---
+++ nodeserver.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const server = express();
+const uploadFolder = "/uploaded_files/";
+
+// View Engine Setup
+server.set("views", path.join(__dirname, "views"));
+server.set("view engine", "ejs");
+	
+server.get("/", function(req, res){
+	res.render("home");
+});
+
+// uploaded files are saved in the folder specified by uploadFolder
+const upload = multer({
+  dest: __dirname + uploadFolder
+});
+
+server.use(express.static('public'));
+
+// "dbfiles" is the key of the http payload
+server.post('/uploadFiles', upload.array('dbfiles', 5), function(request, respond) {
+  if(request.files) {
+	request.files.forEach(file => {
+		console.log(file);
+
+		// save the file
+		var fs = require('fs');
+		fs.rename(__dirname + uploadFolder + file.filename, __dirname + uploadFolder + file.originalname, function(err) {
+			if (err) console.log('ERROR: ' + err);
+		});
+	});
+  }
+
+  respond.end('Files uploaded!');
+});
+
+server.listen(8080, function(error) {
+  if (error) throw error
+  console.log(`Server running at http://localhost:8080/`);
+});
